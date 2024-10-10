090d7748bf780e6804f11f033ebf5f93913a0918
Atheris0 <minibalkabagi@hotmail.com>
2024-10-10 21:31:12 +0300

    exercise 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(testString) {
+    s = testString.split('');
+    s = s.reverse();
+    s = s.join('');
+    return s;
 };
 
 // Do not edit below this line
