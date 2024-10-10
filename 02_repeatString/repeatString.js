affa8004c8256e5ef3425bd7b4761073bb798636
Atheris0 <minibalkabagi@hotmail.com>
2024-10-10 20:37:04 +0300

    exercise 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let result = "";
+    if (num < 0) {
+        return 'ERROR';
+    }
+    while(num > 0){
+        result += word;
+        num--;
+    }
+    return result;
 };
 
 // Do not edit below this line
