ed80eb33e4e7d19ebdde914b2b4e56f40e9f7207
Atheris0 <minibalkabagi@hotmail.com>
2024-10-20 22:38:36 +0300

    exercise 4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+
+  for (let i = arr.length -1; i >= 0; i--){
+
+    if (args.includes(arr[i])) {
+      arr.splice(i, 1); // 2nd parameter means remove one item only
+      }
+  }
+  
+  return arr;
 };
 
 // Do not edit below this line
