1d7af7ceb401c4c259bdbd8de7c5b0ab6074215c
Atheris0 <minibalkabagi@hotmail.com>
2024-10-22 00:15:44 +0300

    exercise 8 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((x, y) => x + y, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((x, y) => x * y);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let pow = a;
+	for (let i = 1; i < b; i++){
+    console.log(pow);
+    pow *= a;
+  }
+	return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  
+  return n < 2 ? 1 : n * factorial(n - 1);
+
 };
 
 // Do not edit below this line
