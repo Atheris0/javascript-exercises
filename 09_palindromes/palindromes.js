3aa4bbc2336d541558376b6445e51beb9a0657e6
Atheris0 <minibalkabagi@hotmail.com>
2024-10-23 23:29:26 +0300

    exercise 9 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    const cleaner = word.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+  
+    const reverser = cleaner.split('').reverse().join('');
+  
+    return cleaner === reverser;
 };
 
 // Do not edit below this line
